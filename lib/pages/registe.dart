c81c8241ec5b96610fceeed974c0bd72fe29f1d6
Kalo <email@example.com>
2020-11-08 20:09:30 +0800

    initial register

---
+++ lib/pages/registe.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import '../../res/userD.dart';
+import '../tabs/tab.dart';
+
+class Registe extends StatefulWidget {
+  Registe({Key key}) : super(key: key);
+
+  @override
+  _Registe createState() => _Registe();
+}
+
+class _Registe extends State<Registe> {
+  TextEditingController tc1 = TextEditingController();
+  TextEditingController tc2 = TextEditingController();
+
+  
+}
