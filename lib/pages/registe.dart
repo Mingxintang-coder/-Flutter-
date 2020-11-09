fc0f217de8164f9d93045bfda47770b8fcce3cdf
Kalo <email@example.com>
2020-11-09 20:11:06 +0800

    add build to register

--- lib/pages/registe.dart
+++ lib/pages/registe.dart
@@ -14,5 +14,48 @@ class _Registe extends State<Registe> {
   TextEditingController tc1 = TextEditingController();
   TextEditingController tc2 = TextEditingController();
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('欢迎注册'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: tc1,
+                  decoration: InputDecoration(
+                    labelText: 'USER',
+                  ),
+                  autofocus: false, //close keyboard
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: tc2,
+                  decoration: InputDecoration(
+                    labelText: 'CODE',
+                  ),
+                  autofocus: false, //close keyboard
+                ),
+              ),
+              RaisedButton(
+                onPressed: _registe,
+                child: Text('Registe'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   
+
+
 }
